9f5a8789613c018985d2190c12dfd6ec6cf49895
camboui <mathias.branger@gmail.com>
2016-10-05 14:26:13 -0400

    update main

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -1,6 +1,5 @@
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -15,7 +14,7 @@ public class Main {
 		double end;
 		double delay;
 		// set framerate
-		double fps = 1000.0 / 30.0;
+		double fps = 1000.0 / 60.0;
 		Window w = new Window(500, 500);
 
 		List<Shape> shapes = new ArrayList<Shape>();
@@ -23,8 +22,12 @@ public class Main {
 		OBB obb = new OBB(new Position(150, 250), 100, 20, Math.PI / 3.0);
 		// shapes.add(obb);
 
-		shapes.add(new Circle(100, 100, 50));
-		shapes.add(new Point(50, 50));
+		Circle sun = new Circle(100, 100, 50);
+		Circle planet = (new Circle(40, 40, 30));
+		shapes.add(planet);
+		shapes.add(sun);
+		shapes.add(new Circle(250, 250, 100));
+
 		// shapes.add(new Circle(300, 100, 100));
 		// shapes.add(new AABB(new Position(10, 10), 100, 20));
 		// shapes.add(new AABB(new Position(10, 10), 10, 200));
@@ -46,9 +49,10 @@ public class Main {
 			start = System.currentTimeMillis();
 
 			// do logic
-			// ApplyLogic(shapes);
+			ApplyLogic(shapes);
 
 			obb.setAngle(obb.getAngle() + 0.1f);
+			planet.setPosition(planet.getPosition().rotation(sun.getPosition(), 0.05f));
 
 			w.drawAll(shapes);
 			end = System.currentTimeMillis();
@@ -69,10 +73,12 @@ public class Main {
 			List<Shape> temp = new ArrayList<Shape>(shapes);
 			temp.remove(current);
 
-			for (Shape shape : temp) {
-				System.out.println("will compare " + current.getClass().getName() + " with " + shape.getClass().getName());
-			}
-			System.out.println(current.isCollide(temp));
+//			for (Shape shape : temp) {
+//				System.out.println(
+//						"will compare " + current.getClass().getName() + " with " + shape.getClass().getName());
+//			}
+			// System.out.println(current.isCollide(temp));
+			current.isCollide(temp);
 		}
 	}
 
